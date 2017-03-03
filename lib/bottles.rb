24072cac695a8749b7fa1b9b1b7e7dd52ba895d3
Chris Legg <chris@navondigital.com>
2017-03-03 13:02:10 +0000

    Implment test 3 and code

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,13 @@
 class Bottles
 
   def verse(number)
-    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-      "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+    case number
+    when 2
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
+        "Take one down and pass it around, #{number-1} bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
+        "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+    end
   end
 end
