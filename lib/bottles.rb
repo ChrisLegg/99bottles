7951023869d747129c9a3aaaa6bd8fe8c652672f
Chris Legg <chris@chrislegg.net>
2017-03-07 21:36:22 +0000

    Refactor to remove case 1 from the switch statement

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -16,16 +16,31 @@ class Bottles
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\n" +
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" +
-        "Take it down and pass it around, no more bottles of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-        "Take one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+      "#{number} #{container(number)} of beer on the wall, " +
+        "#{number} #{container(number)} of beer.\n" +
+        "Take #{pronoun(number)} down and pass it around, " +
+        "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     end
   end
 
-  def container(number=:FIXME)
+  def quantity(number=:FIXME)
+    if number==0
+      "no more"
+    else
+      number
+    end
+  end
+
+  def pronoun(number)
+    if number==1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def container(number)
     if number == 1
       "bottle"
     else 
